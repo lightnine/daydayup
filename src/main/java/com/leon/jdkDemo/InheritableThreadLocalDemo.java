49c41d1f1604b8c00caa6e8fdd22fde4175adc6e
lightnine <llxjtu08@gmail.com>
2020-02-16 12:36:25 +0800

    展示InheritableThreadLocalDemo的使用

---
+++ src/main/java/com/leon/jdkDemo/InheritableThreadLocalDemo.java
@@ -0,0 +1,42 @@
+package com.leon.jdkDemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 展示InheritableThreadLocal的用法
+ * @author leon
+ * @since 2020/2/16 10:36
+ */
+public class InheritableThreadLocalDemo {
+    public static void main(String[] args) throws InterruptedException {
+        ThreadLocal<String> threadLocal = new ThreadLocal<>();
+        InheritableThreadLocal<String> inheritableThreadLocal = new InheritableThreadLocal<>();
+        InheritableThreadLocal<List<String>> inheritableThreadLocal1 = new InheritableThreadLocal<>();
+        threadLocal.set("string1");
+        inheritableThreadLocal.set("string2");
+        List<String> list = new ArrayList<>();
+        list.add("str1");
+        inheritableThreadLocal1.set(list);
+        System.out.println("主线程：" + Thread.currentThread() + threadLocal.get());
+        System.out.println("主线程：" + Thread.currentThread() + inheritableThreadLocal.get());
+        Thread sub = new Thread() {
+            @Override
+            public void run() {
+                System.out.println("子线程：" + Thread.currentThread() + threadLocal.get());
+                System.out.println("子线程：" + Thread.currentThread() + inheritableThreadLocal.get());
+                inheritableThreadLocal.set("string3");
+                System.out.println("子线程：" + Thread.currentThread() + inheritableThreadLocal.get());
+
+                List<String> subList = inheritableThreadLocal1.get();
+                System.out.println("子线程list:" + inheritableThreadLocal1.get());
+                subList.add("str2");
+                inheritableThreadLocal1.set(subList);
+            }
+        };
+        sub.start();
+        sub.join();
+        System.out.println("主线程：" + Thread.currentThread() + inheritableThreadLocal.get());
+        System.out.println("主线程list：" + inheritableThreadLocal1.get());
+    }
+}
